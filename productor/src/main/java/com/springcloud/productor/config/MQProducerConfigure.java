0271e18d56c65c317e00dabd5c145b74c0dd7f76
zhuzs <32271827+zhuzs212@users.noreply.github.com>
2022-09-02 16:25:13 +0800

    【修改】: 1.代码优化；

--- productor/src/main/java/com/springcloud/productor/config/MQProducerConfigure.java
+++ productor/src/main/java/com/springcloud/productor/config/MQProducerConfigure.java
@@ -1,53 +1,53 @@
-package com.springcloud.productor.config;
-
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * mq生产者配置
- *
- * @author zhuzishuang
- * @date 2022/5/23
- */
-@Configuration
-@ConfigurationProperties(prefix = "rocketmq.producer")
-@Data
-@Slf4j
-public class MQProducerConfigure {
-
-    private String groupName;
-    //
-    private String nameServerAddress;
-    // 消息最大值
-    private Integer maxMessageSize;
-    // 消息发送超时时间
-    private Integer sendMsgTimeOut;
-    // 失败重试次数
-    private Integer retryTimesWhenSendFailed;
-
-    /**
-     * mq 生成者配置
-     * @return
-     * @throws MQClientException
-     */
-    @Bean
-    @ConditionalOnProperty(prefix = "rocketmq.producer", value = "isOnOff", havingValue = "on")
-    public DefaultMQProducer defaultProducer() throws MQClientException {
-        log.info("defaultProducer 正在创建---------------------------------------");
-        DefaultMQProducer producer = new DefaultMQProducer(groupName);
-        producer.setNamesrvAddr(nameServerAddress);
-        producer.setVipChannelEnabled(false);
-        producer.setMaxMessageSize(maxMessageSize);
-        producer.setSendMsgTimeout(sendMsgTimeOut);
-        producer.setRetryTimesWhenSendAsyncFailed(retryTimesWhenSendFailed);
-        producer.start();
-        log.info("rocketmq producer server 开启成功----------------------------------");
-        return producer;
-    }
-}
+//package com.springcloud.productor.config;
+//
+//import lombok.Data;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.rocketmq.client.exception.MQClientException;
+//import org.apache.rocketmq.client.producer.DefaultMQProducer;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * mq生产者配置
+// *
+// * @author zhuzishuang
+// * @date 2022/5/23
+// */
+//@Configuration
+//@ConfigurationProperties(prefix = "rocketmq.producer")
+//@Data
+//@Slf4j
+//public class MQProducerConfigure {
+//
+//    private String groupName;
+//    //
+//    private String nameServerAddress;
+//    // 消息最大值
+//    private Integer maxMessageSize;
+//    // 消息发送超时时间
+//    private Integer sendMsgTimeOut;
+//    // 失败重试次数
+//    private Integer retryTimesWhenSendFailed;
+//
+//    /**
+//     * mq 生成者配置
+//     * @return
+//     * @throws MQClientException
+//     */
+//    @Bean
+//    @ConditionalOnProperty(prefix = "rocketmq.producer", value = "isOnOff", havingValue = "on")
+//    public DefaultMQProducer defaultProducer() throws MQClientException {
+//        log.info("defaultProducer 正在创建---------------------------------------");
+//        DefaultMQProducer producer = new DefaultMQProducer(groupName);
+//        producer.setNamesrvAddr(nameServerAddress);
+//        producer.setVipChannelEnabled(false);
+//        producer.setMaxMessageSize(maxMessageSize);
+//        producer.setSendMsgTimeout(sendMsgTimeOut);
+//        producer.setRetryTimesWhenSendAsyncFailed(retryTimesWhenSendFailed);
+//        producer.start();
+//        log.info("rocketmq producer server 开启成功----------------------------------");
+//        return producer;
+//    }
+//}
